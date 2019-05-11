29872587fa5b866e0d001e8306415fdd1477e572
yuki0508 <enla.festa.la.vita@icloud.com>
2019-05-11 14:33:52 +0900

    月間カレンダー

---
+++ CalenderService/app/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from . import views
+
+app_name = 'app'
+
+urlpatterns = [
+    path('', views.MonthCalendar.as_view(), name='month'),
+    path('month/<int:year>/<int:month>/', views.MonthCalendar.as_view(), name='month'),
+]
---
+++ CalenderService/app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import generic
+from . import mixins
+
+
+class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
+    """月間カレンダーを表示するビュー"""
+    template_name = 'app/month.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        calendar_context = self.get_month_calendar()
+        context.update(calendar_context)
+        return context
